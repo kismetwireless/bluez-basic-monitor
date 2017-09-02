f098e7282d5ae35a71157921c714d574b6f2bb82
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-09-01 23:00:33 -0400

    Initial code drop

---
+++ Makefile
@@ -0,0 +1,38 @@
+
+MONITOR_OBJS = \
+	gdbus/client.c.o \
+	gdbus/mainloop.c.o \
+	gdbus/object.c.o \
+	gdbus/polkit.c.o \
+	gdbus/watch.c.o \
+	bluez-monitor.c.o
+MONITOR_BIN = bluez-monitor
+
+CC = gcc
+LD = gcc
+CFLAGS = -I. `pkg-config glib-2.0 --cflags` `pkg-config dbus-glib-1 --cflags`
+LDFLAGS = 
+LIBS = `pkg-config glib-2.0 --libs` `pkg-config dbus-glib-1 --libs` -ldl
+
+all: $(MONITOR_BIN)
+
+$(MONITOR_BIN):	$(MONITOR_OBJS) $(patsubst %c.o,%c.d,$(MONITOR_OBJS))
+		$(LD) $(LDFLAGS) -o $(MONITOR_BIN) $(MONITOR_OBJS) $(LIBS) 
+
+clean:
+	@rm *.o
+	@rm *.d
+	@rm gdbus/*.o
+	@rm gdbus/*.d
+
+%.c.o:	%.c
+%.c.o : %.c %.c.d
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c $*.c -o $@
+
+%.c.d:	%.c
+	$(CC) -MM $(CFLAGS) $(CPPFLAGS) $*.c | sed -e "s/\.o/\.c.o/" > $*.c.d
+
+.PRECIOUS: %.c.d
+
+include $(wildcard $(patsubst %c.o,%c.d,$(MONITOR_OBJS)))
+
---
+++ bluez-monitor.c
@@ -0,0 +1,153 @@
+/* Simplified bluez monitoring interface
+ *
+ *  Originally based on bluez-5.46 client code, 
+ *
+ *  Copyright (C) 2012  Intel Corporation. All rights reserved.
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <sys/signalfd.h>
+#include <string.h>
+
+#include <glib.h>
+
+#include "gdbus/gdbus.h"
+
+static GMainLoop *main_loop;
+static DBusConnection *dbus_connection;
+
+static GDBusProxy *agent_manager;
+
+struct gdbus_bluez_adapter {
+    GDBusProxy *proxy;
+    GList *devices;
+};
+
+
+static void connect_handler(DBusConnection *connection, void *user_data) {
+    fprintf(stderr, "DEBUG - connect_handler\n");
+}
+
+static void handle_device(GDBusProxy *proxy, const char *description) {
+    DBusMessageIter iter;
+    const char *address, *alias;
+
+    if (!g_dbus_proxy_get_property(proxy, "Address", &iter)) {
+        return;
+    }
+
+    dbus_message_iter_get_basic(&iter, &address);
+
+    if (g_dbus_proxy_get_property(proxy, "Alias", &iter)) {
+        dbus_message_iter_get_basic(&iter, &alias);
+    } else {
+        alias = NULL;
+    }
+
+    fprintf(stderr, "DEBUG - device address %s alias %s\n", address, alias);
+}
+
+static void dbus_proxy_added(GDBusProxy *proxy, void *user_data) {
+    const char *interface;
+
+    interface = g_dbus_proxy_get_interface(proxy);
+
+    if (!strcmp(interface, "org.bluez.Device1")) {
+        // device_added(proxy);
+        fprintf(stderr, "debug - proxy - device added\n");
+    } else if (!strcmp(interface, "org.bluez.Adapter1")) {
+        // adapter_added(proxy);
+        fprintf(stderr, "debug - adapter added\n");
+    }
+}
+
+static void dbus_proxy_removed(GDBusProxy *proxy, void *user_data) {
+    const char *interface;
+
+    interface = g_dbus_proxy_get_interface(proxy);
+
+    if (!strcmp(interface, "org.bluez.Device1")) {
+        // device_removed(proxy);
+        fprintf(stderr, "debug - proxy - device removed\n");
+    } else if (!strcmp(interface, "org.bluez.Adapter1")) {
+        // adapter_removed(proxy);
+        fprintf(stderr, "debug - proxy - adapter removed\n");
+    }
+}
+
+static void dbus_property_changed(GDBusProxy *proxy, const char *name,
+        DBusMessageIter *iter, void *user_data) {
+
+    const char *interface;
+
+    interface = g_dbus_proxy_get_interface(proxy);
+
+    if (!strcmp(interface, "org.bluez.Device1")) {
+        DBusMessageIter addr_iter;
+        char *str;
+
+        if (g_dbus_proxy_get_property(proxy, "Address", &addr_iter) == TRUE) {
+            const char *address;
+            dbus_message_iter_get_basic(&addr_iter, &address);
+
+            fprintf(stderr, "debug - property changed device addr %s\n", address);
+        } 
+    } else if (!strcmp(interface, "org.bluez.Adapter1")) {
+        DBusMessageIter addr_iter;
+        char *str;
+
+        if (g_dbus_proxy_get_property(proxy, "Address", &addr_iter) == TRUE) {
+            const char *address;
+
+            dbus_message_iter_get_basic(&addr_iter, &address);
+
+            fprintf(stderr, "debug - controller changed %s\n", address);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    GError *error = NULL;
+    GDBusClient *client;
+
+    main_loop = g_main_loop_new(NULL, FALSE);
+    dbus_connection = g_dbus_setup_bus(DBUS_BUS_SYSTEM, NULL, NULL);
+    g_dbus_attach_object_manager(dbus_connection);
+
+    client = g_dbus_client_new(dbus_connection, "org.bluez", "/org/bluez");
+
+    g_dbus_client_set_proxy_handlers(client, dbus_proxy_added, 
+            dbus_proxy_removed, dbus_property_changed, NULL);
+
+    g_dbus_client_set_ready_watch(client, NULL, NULL);
+
+    g_main_loop_run(main_loop);
+
+    g_dbus_client_unref(client);
+    dbus_connection_unref(dbus_connection);
+    g_main_loop_unref(main_loop);
+
+    return 0;
+}
+
